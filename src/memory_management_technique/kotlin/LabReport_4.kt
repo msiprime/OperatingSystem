a424b8f973916fe42793e8d919430d9719314a45
msiprime <msisakib958@gmail.com>
2023-11-30 14:16:25 +0600

    kt and java both works on lab 4

--- src/memory_management_technique/kotlin/LabReport_4.kt
+++ src/memory_management_technique/kotlin/LabReport_4.kt
@@ -1,4 +1,47 @@
 package memory_management_technique.kotlin
 
-class LabReport_4 {
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    fun inputArray(size: Int, prompt: String) = IntArray(size) {
+        print("$prompt ${it + 1}: ")
+        scanner.nextInt()
+    }
+
+    print("Enter the number of Blocks– ")
+    val numBlocks = scanner.nextInt()
+    val blockSize = inputArray(numBlocks, "Block")
+    val blockSizeCopy = blockSize.copyOf()
+
+    print("Enter the number of processes– ")
+    val numProcesses = scanner.nextInt()
+    val processSize = inputArray(numProcesses, "Enter memory required for process")
+
+    val blockAllocation = IntArray(numBlocks) { -1 }
+
+    for (i in 0 until numProcesses) {
+        val j = blockSize.indexOfFirst { it >= processSize[i] }
+        if (j != -1) {
+            blockAllocation[j] = i
+            blockSize[j] -= processSize[i]
+        }
+    }
+
+    println("Processes\tProcesses size\tBlocks\tBlocks size\tAllocated\tInt. Frag.")
+
+    for (i in 0 until numProcesses) {
+        val allocatedBlock = blockAllocation.indexOfFirst { it == i }
+
+        with(processSize[i]) {
+            print("\t${i + 1}\t\t\t$this\t\t\t")
+            if (allocatedBlock != -1) {
+                print("${allocatedBlock + 1}\t\t\t${blockSizeCopy[allocatedBlock]}\t\t\tYES\t\t\t${blockSize[allocatedBlock]}")
+            } else {
+                print("-\t\t\t-\t\t\tNO\t\t\t-")
+            }
+            println()
+        }
+    }
 }
